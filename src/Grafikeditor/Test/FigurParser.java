baab889c1c442dd52c899a25ad1af188bb2bd8c0
Jonas Cosandey <jonascosandey@hotmail.com>
2016-06-19 15:52:43 +0200

    noch klassen diagramm erstellen

--- src/Grafikeditor/Test/FigurParser.java
+++ src/Grafikeditor/Test/FigurParser.java
@@ -9,37 +9,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FigurParser {
-  private FigurDAO dao;
+    private FigurDAO dao;
 
-  public FigurParser(FigurDAO dao) {
-    this.dao = dao;
-  }
-
-  public List<Figure> parse() {
-    List<Figure> figuren = new ArrayList<Figure>();
-    String[] figurData = dao.readNextFigurData();
-    while (figurData != null) {
-      String figurTyp = figurData[0];
-      int x = Integer.valueOf(figurData[1]);
-      int y = Integer.valueOf(figurData[2]);
-      switch (figurTyp) {
-      case "Rechteck":
-        int breite = Integer.valueOf(figurData[3]);
-        int hoehe = Integer.valueOf(figurData[4]);
-        figuren.add(new RectangleFigure(x, y, breite, hoehe));
-        break;
-      case "Kreis":
-        int radius = Integer.valueOf(figurData[3]);
-        figuren.add(new CircleFigure(x, y, radius));
-        break;
-      case "Linie":
-        int endx = Integer.valueOf(figurData[3]);
-        int endy = Integer.valueOf(figurData[4]);
-        figuren.add(new LineFigure(x, y, endx, endy));
-        break;
-      }
-      figurData = dao.readNextFigurData();
+    public FigurParser(FigurDAO dao) {
+      this.dao = dao;
     }
-    return figuren;
-  }
+
+    public List<Figure> parse() {
+        List<Figure> figuren = new ArrayList<Figure>();
+        String[] figurData = dao.readNextFigurData();
+        while (figurData != null) {
+          String figurTyp = figurData[0];
+          int x = Integer.valueOf(figurData[1]);
+          int y = Integer.valueOf(figurData[2]);
+          switch (figurTyp) {
+          case "Rechteck":
+            int breite = Integer.valueOf(figurData[3]);
+            int hoehe = Integer.valueOf(figurData[4]);
+            figuren.add(new RectangleFigure(x, y, breite, hoehe));
+            break;
+          case "Kreis":
+            int radius = Integer.valueOf(figurData[3]);
+            figuren.add(new CircleFigure(x, y, radius));
+            break;
+          case "Linie":
+            int endx = Integer.valueOf(figurData[3]);
+            int endy = Integer.valueOf(figurData[4]);
+            figuren.add(new LineFigure(x, y, endx, endy));
+            break;
+          }
+          figurData = dao.readNextFigurData();
+        }
+        return figuren;
+    }
 }
